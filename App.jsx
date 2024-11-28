489d8e5ab362208ebd2da176aca31b64ab43f6ac
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-28 20:30:43 +0530

    starting

--- App.jsx
+++ App.jsx
@@ -2,25 +2,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Creating a React element
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [
-        React.createElement(
-            "div",
-            { id: "child" },
-            [
-                React.createElement("h1", {}, "I'm an h1 tag"),
-                React.createElement("h2", {}, "I'm an h2 tag"),
-            ]
-        )
-    ]
-);
+const heading=React.createElement("h1",
+    {id:"heading"},
+    "Namaste React 🥊")
 
-// Logging the React object (Virtual DOM structure)
-console.log("React object (Virtual DOM):", parent);
+const root =ReactDOM.createRoot
+(document.getElementById("root"))
 
-// Rendering the React element into the actual DOM
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading)
