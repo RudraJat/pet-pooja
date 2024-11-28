6fb82430f4dc9afc20d4bb71550228bbcf6604ef
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-28 22:14:15 +0530

    up

--- App.jsx
+++ App.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
 //React.createElement => Object =>HTMLElement(Render)
 
 // const heading=React.createElement("h1",
@@ -11,13 +10,16 @@ import ReactDOM from "react-dom/client";
 
 // console.log(heading);
 
-//Easy Way 
+//Easy Way
 //JSX - HTML-like or XML-like syntax
-const jsxHeading=<h1 id="heading">Namaste React using JSX🥊</h1>
+const jsxHeading = (
+    <h1 className="head">
+        Namaste React using JSX🥊
+    </h1>
+);
 
 console.log(jsxHeading);
 
-const root =ReactDOM.createRoot
-(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(jsxHeading);
