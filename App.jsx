902541cae2da180fb48d2345524ff38eebe38070
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-30 22:18:51 +0530

    up

--- App.jsx
+++ App.jsx
@@ -13,11 +13,24 @@ import ReactDOM from "react-dom/client";
 //Easy Way
 //JSX - HTML-like or XML-like syntax
 
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *   - RestroCard
+ *    - Img
+ *    - Name of restro, Star Rating, cuisine, delivery time
+ */
 const Header =()=>{
     return(
         <div className="header">
             <div className="logo-container">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf3x3BazQ5A8Pl23HsWGaSdWX0iOGwNDqeGw&s" alt="Logo" />
+                <img className="logo"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf3x3BazQ5A8Pl23HsWGaSdWX0iOGwNDqeGw&s" alt="Logo" />
             </div>
             <div className="nav-items">
                 <ul>
@@ -31,11 +44,37 @@ const Header =()=>{
     );
 };
 
+const RestaurantCard = () =>{
+    return(
+        <div className="restro-card" style={{ backgroundColor: "#f0f0f0"}}>
+            <img 
+            className="restro-logo"
+            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" 
+            alt="restro-logo" />
+            <h3>Meghana Food</h3>
+        </div>
+    );
+};
+
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="Search">
+                Search
+            </div>
+            <div className="restro-container">
+                 <RestaurantCard/>
+            </div>
+        </div>
+    );
+};
+
 const AppLayout = () =>{
     return <div className="app">
         <Header/>
+        <Body/>
     </div>
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
  
-root.render(<Header/>);
+root.render(<AppLayout/>);
